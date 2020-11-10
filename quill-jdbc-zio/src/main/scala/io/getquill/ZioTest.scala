adddb6292c03d8576970139ea43131f4ad5744d7
Alexander Ioffe <deusaquilus@gmail.com>
2020-11-09 20:38:32 -0500

    continue

--- quill-jdbc-zio/src/main/scala/io/getquill/ZioTest.scala
+++ quill-jdbc-zio/src/main/scala/io/getquill/ZioTest.scala
@@ -8,6 +8,7 @@ import com.typesafe.config.Config
 import io.getquill.util.LoadConfig
 import javax.sql.DataSource
 import zio._
+import zio.blocking.{Blocking, effectBlocking}
 import zio.internal.Platform
 //import zio.Exit._
 
@@ -15,31 +16,40 @@ case class Person(name: String, age: Int)
 
 object ZioTest {
 
+//  type SqlDataSource = Has[SqlDataSource.Service]
+//  object SqlDataSource {
+//    trait Service {
+//      def dataSource: Task[DataSource]
+//    }
+//    val simple: Layer[Nothing, DataSource] = ZLayer.fromFunction { (ds: DataSource) =>
+//      new Service {
+//        override def dataSource: Task[DataSource] = Task(ds)
+//      }
+//    }
+//
+//    //def postgres = ZIO.accessM { (p: Prefix) => p.get.prefix }
+//  }
+
   def main(args: Array[String]): Unit = {
     val ctx = new PostgresZioJdbcContext(Literal)
     import ctx._
     val runner = io.getquill.context.zio.Runner.default
     import runner._
+    val r = ctx.run(query[Person])
+    val conf = JdbcContextConfig(LoadConfig("postgres"))
+    val print = r.map(r => println(r))
+    //val runtime = Runtime.apply(conf.dataSource.getConnection, Platform.default)
+    //runtime.unsafeRun(print)
 
-//    val prefixEnv = ZIO.environment[String]
-//    val prefixDataSource =
-//      prefixEnv
-//        .mapEffect(LoadConfig(_))
-//        .mapEffect(JdbcContextConfig(_))
-//        .mapEffect(_.dataSource)
-//    val aquired = prefixDataSource.map(_.getConnection)
-//    val withBracketedConn = RIO.bracket(aquired)(c => catchAll(RIO(c.close())))(c => RIO.fromFunction((str: String) => c))
-
-    //ZManaged.make()
-
-    val prefix = ZIO.environment[String]
-    import zio.blocking.Blocking._
-    def block[A](a: =>A) = IO.effect(a)
 
     val prefixToConfig = ZLayer.fromFunctionManyM { (str: String) => IO.effect(LoadConfig(str)) }
     val configToDsConf = ZLayer.fromFunctionManyM { conf: Config => IO.effect(JdbcContextConfig(conf)) }
     val dsConfToDs = ZLayer.fromFunctionManyM { jconf: JdbcContextConfig => IO.effect(jconf.dataSource) }
     val dsToConn = ZLayer.fromAcquireReleaseMany(ZIO.environment[DataSource].mapEffect(_.getConnection).refineToOrDie[SQLException])(c => catchAll(RIO(c.close)))
+
+
+    def fromPrefixService[T](qzio: ZIO[Connection, Throwable, T]) =
+      qzio.provideLayer(prefixToConfig >>> configToDsConf >>> dsConfToDs >>> dsToConn)
 
     def fromPrefix[T](qzio: ZIO[Connection, Throwable, T]) =
       qzio.provideLayer(prefixToConfig >>> configToDsConf >>> dsConfToDs >>> dsToConn)
@@ -50,18 +60,50 @@ object ZioTest {
     def fromDs[T](qzio: ZIO[Connection, Throwable, T]) =
       qzio.provideLayer(dsToConn)
 
+
+//    val svc: ZLayer[Any, Nothing, Prefix with Blocking] = Prefix.simple ++ zio.blocking.Blocking.live
+//
+//
+//
+//    val v = {
+//      zio.blocking.blocking {
+//        for {
+//          prefix <- Prefix.postgres
+//          r <- fromPrefix(r).provide(prefix)
+//        } yield r
+//      }
+//    }
+//    v.provideLayer(svc)
+
+
+
+    //    val prefixEnv = ZIO.environment[String]
+//    val prefixDataSource =
+//      prefixEnv
+//        .mapEffect(LoadConfig(_))
+//        .mapEffect(JdbcContextConfig(_))
+//        .mapEffect(_.dataSource)
+//    val aquired = prefixDataSource.map(_.getConnection)
+//    val withBracketedConn = RIO.bracket(aquired)(c => catchAll(RIO(c.close())))(c => RIO.fromFunction((str: String) => c))
+
+    //ZManaged.make()
+
+    //val prefix = ZIO.environment[String]
+    import zio.blocking._
+
+
+
+    //val blockingService = Blocking.Service.live
+    //import blockingService.blocking
+
+
     // todo try blocking
     //ZLayer.fromAcquireReleaseMany(dsConfToDs.build.useNow.mapEffect(_.getConnection), )
 
 
-    val r = ctx.run(query[Person])
-    val conf = JdbcContextConfig(LoadConfig("postgres"))
 
 
 
-    val print = r.map(r => println(r))
-    val runtime = Runtime.apply(conf.dataSource.getConnection, Platform.default)
-    runtime.unsafeRun(print)
 
     //    def die = Task.die(new RuntimeException("Die from interruption"))
     //    def doQuery() = Task({
